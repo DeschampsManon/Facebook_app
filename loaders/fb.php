7b7d36f4851ae8d9bc8fc048664ae96a3de8883d
Gianni <gianni.azizi@gmail.com>
2016-11-17 17:17:01 +0100

    Masquer la clé secrète

--- loaders/fb.php
+++ loaders/fb.php
@@ -2,7 +2,7 @@
 
 	MyController::$fb = new Facebook\Facebook([
 	  'app_id' => '325674481145757',
-	  'app_secret' => 'ca61e4e5272999d450279ad10d3ae859',
+	  'app_secret' => '',
 	  'default_graph_version' => 'v2.8',
 	]);
 
