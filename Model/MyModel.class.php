66d2e155fda2a04724d4c9d4db80217618c7682b
Gianni <gianni.azizi@gmail.com>
2016-11-18 13:01:37 +0100

    Fixed bugs

--- Model/MyModel.class.php
+++ Model/MyModel.class.php
@@ -1,8 +1,8 @@
 <?php 
 	class MyModel {
 
-		public function __construct() {
-
+		public function __construct() {
+			
 		}
 
 	}
--- loaders/bdd.php
+++ loaders/bdd.php
@@ -1,3 +1,3 @@
 <?php  
-	MyController::$bdd = new PDO('mysql:host=localhost;dbname=facebook', 'root', '');
+	MyController::$bdd = new PDO('mysql:host=localhost;dbname=facebook', 'root', 'root');
 ?>
