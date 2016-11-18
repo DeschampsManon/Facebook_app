02cd120b42e8f061e0dc024895dcb92146848c7d
Gianni <gianni.azizi@gmail.com>
2016-11-18 13:00:17 +0100

    Load template page login.tpl

--- login.php
+++ login.php
@@ -1,13 +1,16 @@
 <?php 
-	
-	require __DIR__.'/loaders/bdd.php';
-	require __DIR__.'/loaders/dependencies.php';
-	require __DIR__.'/loaders/twig.php';
-	require __DIR__.'/loaders/fb.php';
+	ini_set('display_errors','on');
+	error_reporting(E_ALL);
+	require __DIR__.'/loaders/globalLoader.php';
 	
 
-	$helper = $fb->getRedirectLoginHelper();
+	$helper = MyController::$fb->getRedirectLoginHelper();
 	$permissions = ['email'];
-	$loginUrl = $helper->getLoginUrl('http://{your-website}/login-callback.php', $permissions);
+	$loginUrl = $helper->getLoginUrl('http://fb.digital-rooster.fr/login-callback.php', $permissions);
 
+	$vars = array(
+		'loginUrl' => $loginUrl
+	);
+
+	MyController::loadTemplate($vars);
 ?>
