0772ffe13b8ed3e47249a60671ae6f42c832156c
Gianni <gianni.azizi@gmail.com>
2017-01-04 16:24:55 +0100

    add fb with hidden app secret

---
+++ loaders/fb.php
@@ -0,0 +1,8 @@
+<?php 
+	// On stock la variable fb (api facebook) dans une variable statique
+	MyController::$fb = new Facebook\Facebook([
+	  'app_id' => '325674481145757',
+	  'app_secret' => '',
+	  'default_graph_version' => 'v2.8',
+	]);
+?>
