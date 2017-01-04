584282ba548319af7c575a3051b1ad32913cba03
Gianni <gianni.azizi@gmail.com>
2017-01-04 16:06:33 +0100

    fix bug

--- loaders/twig.php
+++ loaders/twig.php
@@ -3,7 +3,7 @@
 	require_once __DIR__.'/../vendor/twig/extensions/lib/Twig/Extensions/Autoloader.php';
     Twig_Autoloader::register();
     
-    $loader = new Twig_Loader_Filesystem('Views'); // Dossier contenant les templates
+    $loader = new Twig_Loader_Filesystem('views'); // Dossier contenant les templates
     MyController::$twig = new Twig_Environment($loader, array(
       'cache' => false
     ));
