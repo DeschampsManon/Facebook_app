351c60a73b4ced8f529cd9fdf4a46db26f2723b8
Gianni <gianni.azizi@gmail.com>
2017-01-04 16:21:51 +0100

    add .idea/

--- loaders/fb.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	// On stock la variable fb (api facebook) dans une variable statique
-	MyController::$fb = new Facebook\Facebook([
-	  'app_id' => '325674481145757',
-	  'app_secret' => '',
-	  'default_graph_version' => 'v2.8',
-	]);
-
-?>
