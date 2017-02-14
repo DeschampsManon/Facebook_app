be678f921b2f620aab8c22c1cf366cd987ab756e
Gianni <gianni.azizi@gmail.com>
2017-02-14 15:47:13 +0100

    bug excel correction

--- export.php
+++ export.php
@@ -75,6 +75,8 @@
         $request = MyController::$bdd->query('SELECT first_name, name FROM users WHERE id_user = '. $photo['id_user']);
         $result = $request->fetch(PDO::FETCH_ASSOC);
 
+        if($countLikes == "") { $countLikes = 0 }
+
         $resultats[$count]['user'] = $result['first_name'] . ' ' . $result['name'];
         $resultats[$count]['link_photo'] = $photo['link_photo'];
         $resultats[$count]['likes'] = $countLikes;
