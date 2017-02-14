c981fc2b0864310825757fb55b0bc1bb2d22d6b4
Manon <manon.deschamps@plezi.co>
2017-02-14 16:28:56 +0100

    remove app secret

--- loaders/fb.php
+++ loaders/fb.php
@@ -2,7 +2,7 @@
 	// On stock la variable fb (api facebook) dans une variable statique
 	MyController::$fb = new Facebook\Facebook([
 	  'app_id' => '325674481145757',
-	  'app_secret' => 'b07143c0ac82e515af99ca29cd3b1c55',
+	  'app_secret' => '',
 	  'default_graph_version' => 'v2.8',
 	]);
 
